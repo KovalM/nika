891ac8b5a90d35bcae6e40da017381cf4d723a95
VerogoR <gerogorg.76@gmail.com>
2023-10-25 20:15:49 +0300

    add agent base

--- problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
+++ problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
@@ -1,7 +1,8 @@
 from sc_kpm import ScModule
 from .WeatherAgent import WeatherAgent
+from .GettingObjectsFromMap import MapsObjectsInfoAgent
 
 
 class MessageProcessingModule(ScModule):
     def __init__(self):
-        super().__init__(WeatherAgent())
+        super().__init__(WeatherAgent(), MapsObjectsInfoAgent())
